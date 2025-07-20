a22f8a0782f49489242de9d48b5fd5fb6e95a4c0
Lorr-etta <laurettaamankwah@gmail.com>
2025-07-20 10:54:27 +0000

    Add simple company website built with React

---
+++ my-company/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <Router>
+      {/* This Navbar will be visible on all pages */}
+      <Navbar />
+
+      {/* These are your page routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
