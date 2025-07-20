61439364ff66652628a1e477b82efb2c94406c2d
Lorr-etta <laurettaamankwah@gmail.com>
2025-07-20 08:01:27 +0000

    Add inline styled React app in alx-react-app-new

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#333', color: '#fff', textAlign: 'center', padding: '10px' }}>
+      <p>© 2025 All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
