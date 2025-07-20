3ae0cadc7ba9de57c6163e2f5f1b30703af769ff
Lorr-etta <laurettaamankwah@gmail.com>
2025-07-20 10:25:05 +0000

    Use UserContext in UserProfile instead of prop

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,16 @@
-const UserProfile = (props) => {
+import React, { useContext } from 'react';
+import UserContext from './UserContext';
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+      <h2>User Profile</h2>
+      <p><strong>Name:</strong> {userData.name}</p>
+      <p><strong>Email:</strong> {userData.email}</p>
     </div>
   );
-};
+}
 
 export default UserProfile;
