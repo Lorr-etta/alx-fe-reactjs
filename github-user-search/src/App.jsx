4b8515ef373af237778678bb4df057bc726c8ee2
Lorr-etta <laurettaamankwah@gmail.com>
2025-08-03 12:14:25 +0000

    Setup Tailwind CSS in React project

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
+import Search from './components/Search'; // Import the Search component
 
 function App() {
   return (
     <div className="app">
       <h1>GitHub User Search</h1>
       <p>Start typing to search for GitHub profiles...</p>
+      
+      <Search /> {/* Render the Search component */}
     </div>
   );
 }
