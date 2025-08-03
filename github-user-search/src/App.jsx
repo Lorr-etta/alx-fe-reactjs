a8629118d412570a369c3df67862c508a36e41f1
Lorr-etta <laurettaamankwah@gmail.com>
2025-08-03 10:45:21 +0000

    Update README with project description

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>GitHub User Search</h1>
+      <p>Start typing to search for GitHub profiles...</p>
+    </div>
+  );
+}
+
+export default App;
